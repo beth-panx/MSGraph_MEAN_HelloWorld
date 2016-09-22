505a05307a33fe22389e92f587425a4625587849
beth-panx <beth_panx@hotmail.com>
2016-09-22 15:46:32 -0700

    Added disclaimer. Changed snapchot to use new graph image. Changed email content.

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
 module.exports = {
  	creds: {
     callbackURL: 'https://local.vroov.com:8443/token',
